e80b4e8fe86478d2c241c14ac3727908438931d0
JakobGutmann <jakobgutmann@gmail.com>
2024-01-17 14:33:18 +0100

    update readme

--- README.md
+++ README.md
@@ -1,29 +1,66 @@
-# Script for fairness paper
+# Pricing Parking for Fairness - A Simulation Study Based on an Empirically Calibrated Model of Parking Behavior
+
+<img src="radar_plot.png" width="600">
+
+This repository allows for the replication of all experiments conducted for the paper "Pricing Parking for Fairness - A Simulation Study Based on an Empirically Calibrated Model of Parking Behavior" by Jakob Kappenberger, Heiner Stuckenschmidt, and Frederic Gerdon.
+
+*Please note: To protect the privacy of the respondents participating in our survey, we include a synthetic random data set instead of the actual data collected.*
 
 ## Setup
 *Before beginning, make sure to have 
- [NetLogo 6.2](https://ccl.northwestern.edu/netlogo/download.shtml) installed. On Windows, the installation should be detected automatically. Linux users must specify the respective path.*
+ [NetLogo 6.3](https://ccl.northwestern.edu/netlogo/download.shtml) installed. On Windows, the installation should be detected automatically. Linux users must specify the respective path.*
 ```
 # Build Environment
 cd project_folder
 conda env create -f environment.yml
 # Activate Environment
-conda activate thesis_gutmann
+conda activate parking-fairness
+# Install package
+pip install -e .
 ```
 
 ## Using this Repository
 
-```1
-# To run simulation with turtle documentation
-# The runs from which the pricing is to derived must be moved to 'project_folder/src/runs_for_prices'
-cd project_folder/src
-python run_simulation.py 1
+**Baseline:**
+
+```
+# To run baseline for 100 episodes
+cd project_folder/scripts
+python run_baseline.py 10 --static
 ```
 
 - **episodes** (required): Number of episodes to run the baseline for
 - **--[m]odel_size**: Size of the NetLogo grid to use (either "training" or "evaluation"(default))
+- **--static**: Use static baseline pricing (default False)
 - **--[n]etlogo_[p]ath**: Path to NetLogo installation (for Linux users only)
 - **--gui**: Boolean for NetLogo UI (default False)
 
 All results are written to the baseline subfolder in the experiments directory with a dedicated timestamp to identify them.
+
+**RL-based Pricing:**
+
+```
+# To run RL pricing for 100 episodes on 2 cores with the equity reward function and no group-specific pricing
+cd project_folder/scripts
+python run_experiment.py ppo_agent_local.json 100 -p 2  --no-group_pricing --adjust_free -r equity --document --eval --early_stopping --normalize --fine_tuning --eval_episodes 3
+```
+
+- **episodes** (required): Number of episodes to train for
+- **agent** (required): Path to agent JSON config file
+- **--num_[p]arallel**: CPU cores to use, defaults to 1
+- **--[r]eward_key**: Reward function to use ("occupancy" (default), "n_cars", "equity", "speed", "composite")
+- **--[c]heckpoint**: Checkpoint of previous training process, either used to resume training or for evaluation (currently deprecated)
+- **--[m]odel_size**: Size of the NetLogo grid to use (either "training"(default) or "evaluation")
+- **--[n]etlogo_[p]ath**: Path to NetLogo installation (for Linux users only)
+- **--document**: Save plots for min, median and max performances, defaults to True
+- **--group-pricing**: Use group-specific pricing for different CPZs.
+- **--adjust_free**: Let agent adjust prices freely in interval between 0 and 10, defaults to True
+- **--eval**: Run one model instance in evaluation mode, defaults to False
+- **--zip**: Zip directory of run after experiment is finished, defaults to False
+- **--gui**: Boolean for NetLogo UI (default False)
+- **--early_stopping**: Use early stopping for training.
+- **--normalize**: Use normalization for state space and rewards.
+- **--fine_tuning**: Fine-tune model learned on evaluation model.
+
+All results are written to a subfolder depending on the respective reward function in the experiments directory with a dedicated timestamp to identify them.
 
--- src/experiment.py
+++ src/experiment.py
@@ -12,22 +12,35 @@ import seaborn as sns
 from cmcrameri import cm
 import wandb
 
-from src.custom_environment import CustomEnvironment
-from src.external.tensorforce.execution import Runner
-from src.util import label_episodes, delete_unused_episodes
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv, VecNormalize
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import (
+    EvalCallback,
+    StopTrainingOnNoModelImprovement,
+)
+from stable_baselines3.common.monitor import load_results
+
+from wandb.integration.sb3 import WandbCallback
+
+from src.parking_environment import ParkingEnvironment
+from src.util import label_episodes, delete_unused_episodes, linear_schedule
 
 sns.set_style("dark")
 sns.set_context("paper")
+
+SEED = 2023
 
 
 class Experiment:
     def __init__(
         self,
         agent: str,
-        num_episodes: int,
+        train_episodes: int,
         args,
-        batch_agent_calls: bool = False,
-        sync_episodes: bool = False,
+        model_path: str,
+        eval_episodes: int = 50,
         document: bool = True,
         wandb_project: str = None,
         wandb_entity: str = None,
@@ -36,21 +49,22 @@ class Experiment:
         num_parallel: int = 1,
         reward_key: str = "occupancy",
         checkpoint: str = None,
-        use_newest_checkpoint: bool = False,
         eval: bool = False,
+        early_stopping: bool = False,
+        normalize: bool = False,
+        fine_tuning: bool = False,
         zip: bool = False,
         test: bool = False,
         model_size: str = "training",
         nl_path: str = None,
-        gui: bool = False,
+        render_mode: str = None,
     ):
         """
         Class to run individual experiments.
         :param agent: Agent specification (Path to JSON-file)
-        :param num_episodes: Number of episodes to run.
+        :param train_episodes: Number of episodes for training.
+        :param eval_episodes: Number of episodes for evaluating.
         :param args: Arguments experiment has been called with.
-        :param batch_agent_calls: Whether agent calls are run in batches.
-        :param sync_episodes: Whether all parallel environment are synced.
         :param document: Boolean if model outputs are to be saved.
         :param wandb_entity: Name of Weights and Biases Entity results are logged to.
         :param wandb_project: Name of Weights and Biases Project results are logged to.
@@ -59,161 +73,252 @@ class Experiment:
         :param num_parallel: Number of environments to run in parallel.
         :param reward_key: Key to choose reward function.
         :param checkpoint: Timestamp of checkpoint to resume.
-        :param use_newest_checkpoint: Whether to use the newest checkpoint for the given reward function.
         :param eval: Whether to use one core for evaluation (necessary for evaluation phase).
+        :param early_stopping: Whether to use early stopping, if no improvements are made after x episodes.
+        :param normalize: Whether to normalize states and rewards (only during training).
+        :param fine_tuning: Whether to finetune trained model on eval simualtion.
         :param zip: Whether to zip the experiment directory.
         :param test: Whether to run in test mode.
+        :param model_path: Path to model.
         :param model_size: Model size to run experiments with, either "training" or "evaluation".
         :param nl_path: Path to NetLogo directory (for Linux users).
-        :param gui: Whether NetLogo UI is shown during episodes.
+        :param render_mode: Whether NetLogo UI is shown during episodes (value "human" or None (default)).
         """
 
-        self.num_episodes = num_episodes
-        self.batch_agent_calls = batch_agent_calls
-        self.sync_episodes = sync_episodes
+        self.train_episodes = train_episodes
+        self.eval_episodes = eval_episodes
         self.eval = eval
+        self.early_stopping = early_stopping
+        self.normalize = normalize
+        self.fine_tuning = fine_tuning
         self.zip = zip
         self.document = document
         self.num_parallel = num_parallel
-        assert (
-            not eval or self.num_parallel > 1
-        ), "Need parallel execution for evaluation mode."
+        with open(agent, "r") as fp:
+            self.ppo_config = json.load(fp=fp)
+
         # Check if checkpoint is given (resume if given)
-        if checkpoint is not None and not use_newest_checkpoint:
+        if checkpoint is not None:
             self.resume_checkpoint = True
             self.timestamp = checkpoint
-        elif checkpoint is None and use_newest_checkpoint:
-            self.resume_checkpoint = True
-            runs = glob(
-                str(Path(".").absolute().parent / "results" / reward_key) + "/*"
-            )
-            latest_run = max(runs, key=os.path.getctime).split("\\")[-1]
-            self.timestamp = latest_run
-            print(f"Resuming latest checkpoint {self.timestamp}")
         else:
             self.resume_checkpoint = False
             self.timestamp = datetime.now().strftime("%y%m-%d-%H%M")
 
         self.outpath = (
-            Path(".").absolute().parent / "results" / reward_key / self.timestamp
+            Path(".").absolute().parent
+            / "results"
+            / reward_key
+            / ("group" if group_pricing else "zone")
+            / self.timestamp
         )
         # Create directory (if it does not exist yet)
         self.outpath.mkdir(parents=True, exist_ok=True)
 
-        if self.resume_checkpoint:
-            agent = dict()
-            agent["directory"] = str(self.outpath / "model-checkpoints")
-            agent["format"] = "checkpoint"
-        else:
-            # Load json file for agent init
-            with open(agent, "r") as fp:
-                agent = json.load(fp=fp)
-            # Update checkpoint path
-            agent["saver"]["directory"] = str(self.outpath / "model-checkpoints")
+        self.env_kwargs = {
+            "timestamp": self.timestamp,
+            "reward_key": reward_key,
+            "document": self.document,
+            "adjust_free": adjust_free,
+            "group_pricing": group_pricing,
+            "model_size": model_size,
+            "nl_path": nl_path,
+            "model_path": model_path,
+            "render_mode": render_mode,
+            "test": test,
+        }
 
-            # Document Config
-            args["agent"] = agent
-            with open(str(self.outpath / "config.txt"), "w") as outfile:
-                json.dump(args, outfile)
+        # register Gym Environment
+        eval_env = make_vec_env(
+            ParkingEnvironment,
+            env_kwargs=self.env_kwargs,
+            n_envs=1,
+            seed=SEED,
+            vec_env_cls=SubprocVecEnv,
+        )
+        if self.normalize:
+            eval_env = VecNormalize(eval_env, norm_reward=False, training=False)
 
-        if "summarizer" in agent.keys():
-            agent["summarizer"]["directory"] = str(self.outpath / "summarizer")
+        self.callbacks = [
+            EvalCallback(
+                eval_env,
+                best_model_save_path=str(self.outpath / "log" / "eval" / "model"),
+                log_path=str(self.outpath / "log" / "eval"),
+                eval_freq=5 * self.ppo_config["n_steps"],
+                deterministic=True,
+                render=False,
+                callback_after_eval=StopTrainingOnNoModelImprovement(
+                    max_no_improvement_evals=30, min_evals=50, verbose=1
+                )
+                if self.early_stopping
+                else None,
+            )
+        ]
 
         if wandb_project is not None:
             self.wandb = wandb.init(
                 dir=self.outpath,
                 job_type="eval" if self.resume_checkpoint else "training",
-                name=f"{reward_key}_{'group' if group_pricing else 'zone'}_{'eval' if checkpoint is not None else 'training'}",
+                name=f"{self.timestamp}_{reward_key}_{'group' if group_pricing else 'zone'}_{'eval' if checkpoint is not None else 'training'}",
                 project=wandb_project,
                 entity=wandb_entity,
                 config=args,
-                sync_tensorboard=True if "summarizer" in agent.keys() else False,
+                sync_tensorboard=True,
+            )
+            self.callbacks.append(
+                WandbCallback(
+                    gradient_save_freq=1000,
+                    model_save_path=str(self.outpath / "log" / "eval" / "model"),
+                    verbose=2,
+                )
             )
         else:
             self.wandb = None
 
-        env_kwargs = {
-            "timestamp": self.timestamp,
-            "reward_key": reward_key,
-            "document": self.document,
-            "wandb": self.wandb,
-            "adjust_free": adjust_free,
-            "group_pricing": group_pricing,
-            "model_size": model_size,
-            "nl_path": nl_path,
-            "gui": gui,
-            "test": test,
-        }
-        # Create appropriate number of environments
-        if num_parallel > 1:
-            self.runner = Runner(
-                agent=agent,
-                environment=CustomEnvironment,
-                remote="multiprocessing",
-                evaluation=self.eval,
-                num_parallel=num_parallel,
-                max_episode_timesteps=24,
-                **env_kwargs,
+        self.venv = make_vec_env(
+            ParkingEnvironment,
+            env_kwargs=self.env_kwargs,
+            n_envs=self.num_parallel,
+            seed=SEED,
+            monitor_dir=str(self.outpath / "log"),
+            vec_env_cls=SubprocVecEnv,
+            vec_env_kwargs={
+                "start_method": "spawn"
+            },  # Jpype can only handle spawn processes without pipe errors
+        )
+        if self.normalize:
+            self.venv = VecNormalize(self.venv)
+        if (
+            "lr_schedule" in self.ppo_config.keys()
+            and self.ppo_config["lr_schedule"] == "linear"
+        ):
+            self.ppo_config["learning_rate"] = linear_schedule(
+                self.ppo_config["learning_rate"]
             )
-        else:
-            self.runner = Runner(
-                agent=agent,
-                environment=CustomEnvironment,
-                max_episode_timesteps=24,
-                **env_kwargs,
-            )
-            self.batch_agent_calls = False
-            self.sync_episodes = False
+            # Remove lr schedule from ppo_config dictionary
+            del self.ppo_config["lr_schedule"]
+
+        self.model = PPO(
+            "MlpPolicy",
+            env=self.venv,
+            verbose=1,
+            tensorboard_log=str(self.outpath / "log" / "tensorboard"),
+            # Add arguments from JSON file (learning rate, etc.)
+            **self.ppo_config,
+        )
 
     def run(self):
         """
         Runs actual experiments and saves results.
         :return:
         """
-        print(f"Training for {self.num_episodes} episodes")
-        self.runner.run(
-            num_episodes=self.num_episodes,
-            batch_agent_calls=self.batch_agent_calls,
-            sync_episodes=self.sync_episodes,
-            evaluation=self.eval if self.num_parallel == 1 else False,
-            save_best_agent=str(self.outpath / "best_agent"),
+        print(f"Training for {self.train_episodes} episodes")
+
+        self.model.learn(
+            total_timesteps=self.train_episodes * 24,
+            progress_bar=True,
+            callback=self.callbacks,
+            tb_log_name="training",
         )
+        if self.normalize:
+            self.venv.save(str(self.outpath / "log" / "norm_stats.pkl"))
+        self.venv.close()
+
+        if self.fine_tuning:
+            print(f"Fine tuning for {self.train_episodes / 10} episodes")
+            self.env_kwargs["model_size"] = "evaluation"
+            self.venv = make_vec_env(
+                ParkingEnvironment,
+                env_kwargs=self.env_kwargs,
+                n_envs=self.num_parallel,
+                seed=SEED,
+                monitor_dir=str(self.outpath / "log"),
+                vec_env_cls=SubprocVecEnv,
+                vec_env_kwargs={
+                    "start_method": "spawn"
+                },  # Jpype can only handle spawn processes without pipe errors
+            )
+            self.venv = VecNormalize.load(
+                str(self.outpath / "log" / "norm_stats.pkl"), self.venv
+            )
+            self.model.set_env(self.venv)
+            self.model.learn(
+                total_timesteps=(self.train_episodes / 10) * 24,
+                progress_bar=True,
+                callback=self.callbacks,
+                tb_log_name="fine_tuning",
+            )
+            if self.normalize:
+                self.venv.save(str(self.outpath / "log" / "norm_stats.pkl"))
+            self.venv.close()
 
         # Saving results
         self.save_results()
-        if self.eval:
-            self.save_results(mode="eval")
 
-        # Delete unused episodes
+        if self.eval:
+            print(f"Evaluating for {self.eval_episodes} episodes")
+            self.env_kwargs["eval"] = True
+            self.env_kwargs["document"] = True
+            self.env_kwargs["model_size"] = "evaluation"
+            eval_env = make_vec_env(
+                ParkingEnvironment,
+                env_kwargs=self.env_kwargs,
+                n_envs=self.num_parallel,
+                seed=SEED,
+                vec_env_cls=SubprocVecEnv,
+            )
+
+            best_model_path = self.outpath / "log" / "eval" / "model" / "best_model.zip"
+
+            if best_model_path.is_file() and not self.fine_tuning:
+                self.model.load(best_model_path)
+                print("Best eval model loaded!")
+
+            if self.normalize:
+                eval_env = VecNormalize.load(
+                    str(self.outpath / "log" / "norm_stats.pkl"), eval_env
+                )
+                #  do not update them at test time
+                eval_env.training = False
+                # reward normalization is not needed at test time
+                eval_env.norm_reward = False
+            returns = evaluate_policy(
+                model=self.model,
+                env=eval_env,
+                n_eval_episodes=self.eval_episodes,
+                deterministic=True,
+                return_episode_rewards=True,
+            )
+            print(returns)
+            self.save_results(mode="eval", eval_results=returns)
+
+        # # Delete unused episodes
         if self.document:
             delete_unused_episodes(self.outpath)
 
-        # Close runner
-        self.runner.close()
-
         # Save Experiment output to Weights and Biases
         if self.wandb is not None:
-            self.wandb.save(f"{str(self.outpath)}/*", policy="now")
+            # self.wandb.save(f"{str(self.outpath)}/*", policy="now")
+            self.wandb.finish()
 
         # Zip experiment directory
         if self.zip:
             shutil.make_archive(str(self.outpath), "zip", self.outpath)
             print("directory zipped")
 
-    def save_results(self, mode="training"):
+    def save_results(self, mode="training", eval_results=None):
         """
         Saves results, result plots and, possibly, episode results of experiment.
         :param mode: Either "training" or "evaluation".
         :return:
         """
-        # Accessing the appropriate metrics from runner
         if mode == "training":
-            rewards = np.asarray(self.runner.episode_returns)
-            episode_length = np.asarray(self.runner.episode_timesteps)
-        elif mode == "eval":
-            rewards = np.asarray(self.runner.evaluation_returns)
-            episode_length = np.asarray(self.runner.evaluation_timesteps)
-
+            result_df = load_results(str(self.outpath / "log"))
+            rewards = result_df["r"].values
+            episode_length = result_df["l"].values
+        else:
+            rewards = np.array(eval_results[0])
+            episode_length = np.array(eval_results[1])
         mean_reward = rewards / episode_length
         metrics_df = pd.DataFrame.from_dict(
             {
@@ -223,11 +328,17 @@ class Experiment:
             }
         )
 
-        csv_path = self.outpath / f"{mode}_result_{self.num_episodes}.csv"
+        csv_path = (
+            self.outpath
+            / f"{mode}_result_{self.train_episodes if mode=='training' else self.eval_episodes}.csv"
+        )
         i = 1
         # Check if results file already exists
         while csv_path.is_file():
-            csv_path = self.outpath / f"{mode}_result_{self.num_episodes} ({i}).csv"
+            csv_path = (
+                self.outpath
+                / f"{mode}_result_{self.train_episodes if mode=='training' else self.eval_episodes} ({i}).csv"
+            )
             i += 1
 
         metrics_df.to_csv(str(csv_path))
@@ -254,13 +365,16 @@ class Experiment:
         ax.tick_params(axis="y", labelsize=25)
         ax.tick_params(axis="x", labelsize=25)
 
-        pdf_path = self.outpath / f"{mode}_result_reward_plot_{self.num_episodes}.pdf"
+        pdf_path = (
+            self.outpath
+            / f"{mode}_result_reward_plot_{self.train_episodes if mode=='training' else self.eval_episodes}.pdf"
+        )
         i = 1
         # Check if results file already exists
         while pdf_path.is_file():
             pdf_path = (
                 self.outpath
-                / f"{mode}_result_reward_plot_{self.num_episodes} ({i}).pdf"
+                / f"{mode}_result_reward_plot_{self.train_episodes if mode=='training' else self.eval_episodes} ({i}).pdf"
             )
             i += 1
 
